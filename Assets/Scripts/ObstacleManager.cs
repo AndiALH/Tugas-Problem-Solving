3f8937b643cc2421b0940949b34a83920ba1f363
DESKTOP-221O1M5\Ludy <ludyaldy@gmail.com>
2021-10-09 18:45:30 +0700

    Push problem 8

--- Assets/Scripts/ObstacleManager.cs
+++ Assets/Scripts/ObstacleManager.cs
@@ -5,14 +5,13 @@ using UnityEngine;
 public class ObstacleManager : MonoBehaviour
 {
     // Manager obstacle yang bertugas melakukan spawn obstacle
-    // Script versi problem 7
+    // Script versi problem 8
     // addition:
-    // - Membuat scipt menjadi singleton
-    // - Menambahkan bool apakah object destroyable untuk pengimplementasian menghilangkan object agar problem sebelumnya tidak terpengaruh
-    // - Memasukkan fungsi mendapatkan random position menjadi sebuah method yaitu GetRandomCoordinate(), karena mungkin akan dipakai lagi selanjutnya
-    // - Memasukkan fungsi mengecek apakah area spawn akan collide menjadi sebuah method yaitu CheckSpawnCollide(Vector3, float), karena mungkin akan dipakai lagi selanjutnya
-    // - Membuat fungsi HitObstacle(GameObject) untuk memanggil fungsi menghancurkan obstacle dan menambahkan score pemain
-    // - Membuat fungsi DestroyObstacle(GameObject) yang akan menghancurkan obstacle (dengan cara dinonaktifkan)
+    // - Menambahkan bool apakah object yang di destroy akan di spawn kembali untuk pengimplementasian spawn ulang object agar problem sebelumnya tidak terpengaruh
+    // - Menambah variabel jumlah obstacle yang ingin di spawn dengan default 0. Jika tidak diubah, maka jumlah obstacle yang di spawn akan tetap random dari 3-10
+    // - Menambahkan variabel waktu spawn kembali obstacle yang telah di destroy. Nilai default 3
+    // - Menambahkan kondisi di update yang mengecek apakah ada obstacle yang telah di destroy. Jika ada, maka akan dipanggil fungsi untuk spawn kembali obstacle
+    // - Menambahkan fungsi coroutine SpawnDestroyedObstacle(GameObject, int) yang akan spawn obstacle kembali dengan posisi random dengan delay waktu yang ditentukan
 
     // Menyimpan batas-batas area game
     [SerializeField] private Transform leftBorder;
@@ -28,8 +27,17 @@ public class ObstacleManager : MonoBehaviour
 
     private float area_padding = 0.4f;
 
+    // Menyimpan berapa obstacle yang akan di spawn di awal. Jika nilai kurang dari sama dengan 0, maka jumlah akan tetap random dari 3-10 
+    [SerializeField] private int obstacleCount = 0;
+
+    // Waktu spawn back time obstacle yang telah di destroy sebelumnya
+    [SerializeField] private int spawnBackTime = 3;
+
     // Menyimpan apakah obstacle destroyable, di set false secara default untuk problem dimana obstacle masih tidak destroyable. Untuk problem selanjutnya diset true manual pada editor
     [SerializeField] private bool destroyableObstacle = false;
+
+    // Menyimpan apakah obstacle yang telah di destroy akan di spawn kembali kemudian. Akan dipakai seperti variabel destroyableObstacle
+    [SerializeField] private bool spawnDestroyed = false;
 
     // List untuk destroyed object
     private List<GameObject> destroyedObstacle;
@@ -66,8 +74,9 @@ public class ObstacleManager : MonoBehaviour
         max_x = rightBorder.position.x - area_padding;
         max_y = topBorder.position.y - area_padding;
 
-        // Objek akan di spawn dengan jumlah random dengan range 3 sampai 10
-        int obstacleCount = Random.Range(3, 10);
+        // Objek akan di spawn dengan jumlah random dengan range 3 sampai 10, jika jumlah tidak ditentukan sebelumnya (masih 0)
+        if (obstacleCount <= 0)
+            obstacleCount = Random.Range(3, 11);
         Debug.Log("Obstacle Spawned: " + obstacleCount);
         for (int i = 0; i < obstacleCount; i++)
         {
@@ -76,7 +85,18 @@ public class ObstacleManager : MonoBehaviour
     }
 
     // Update is called once per frame
-    //void Update(){}
+    void Update()
+    {
+        // Dicek setiap update apakah ada obstacle yang telah di destroy untuk di spawn kembali
+        if (spawnDestroyed && destroyedObstacle.Count > 0)
+        {
+            // Dari list yang menyimpan destroyed obstacle, obstacle diremove dari list dan akan di spawn kembali dalam waktu 3 detik
+            GameObject obstacle = destroyedObstacle[0];
+            destroyedObstacle.RemoveAt(0);
+            // Coroutine untuk spawn kembali objek dengan posisi random dan delay sesuai waktu yang dimasukkan
+            StartCoroutine(SpawnDestroyedObstacle(obstacle, spawnBackTime));
+        }
+    }
 
     void SpawnRandom()
     {
@@ -138,6 +158,33 @@ public class ObstacleManager : MonoBehaviour
         obstacle.SetActive(false);
         // Setelah object dinonaktifkan, dimasukkan ke dalam list yang menyimpan destroyed object 
         destroyedObstacle.Add(obstacle);
+    }
+
+    private IEnumerator SpawnDestroyedObstacle(GameObject obstacle, int time)
+    {
+        // Fungsi coroutine untuk spawn kembali objectb yang telah dinonaktifkan sebelumnya dengan posisi baru yang random, dan delay time
+        
+        yield return new WaitForSeconds(time);
+
+        // Untuk mengecek apakah area spawn yang didapatkan masih belum clear atau sudah. Jika belum maka akan dicari posisi lain untuk spawn sampai dapat
+        bool spawnNotClear = false;
+        Vector3 spawn_pos = GetRandomCoordinate();
+        while (!spawnNotClear)
+        {
+            // Jika area spawn di cek terdapat collide, akan dicari posisi random baru
+            if (CheckSpawnCollide(spawn_pos, 0.3f))
+            {
+                spawn_pos = GetRandomCoordinate();
+                continue;
+            }
+            // Jika tidak ada collide, maka area spawn sudah clear
+            spawnNotClear = true;
+        }
+
+        // Mengganti posisi objek menjadi posisi baru, lalu objek kembali diaktifkan
+        obstacle.transform.position = spawn_pos;
+        obstacle.SetActive(true);
+        //Debug.Log("Object spawned back after " + time + " seconds");
     }
 
     // referensi:
